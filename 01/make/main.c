67e0acc28f9506d2d049830f52c59fd5053fffaf
jikim@imguru.co.kr <jikim@imguru.co.kr>
2023-11-26 13:11:08 +0000

    second

--- 01/make/main.c
+++ 01/make/main.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 #include <pthread.h>
-
-void * foo(void *data)
-{
-	int ret;
-	ret = add(3,4);
-	printf("foo(), ret=%d\n", ret);
-	return 0;
-}
 
 int main()
 {
-	int ret;
-	pthread_t thread;
-	pthread_create( &thread , 0, foo, 0 );
-	pthread_join( thread , 0 );
+	printf("%d\n", add(3,4));
 	return 0;
 }
 
